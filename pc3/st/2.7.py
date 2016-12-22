bf47bd7f408ac7145184cd01530234e4604f0a73
Reid <reidddchan@outlook.com>
2016-12-23 00:33:16 +0800

    PYTHON-00031 PC3-02: Specifying a Regular Expression for the Shortest Match

---
+++ pc3/st/2.7.py
@@ -0,0 +1,12 @@
+"""
+Specifying a Regular Expresssion for the Shortest Match
+"""
+import re
+
+str_pat = re.compile(r'\"(.*)\"') # attempting to match text enclosed inside quotes.
+text1 = 'Computer says "no."'
+print(str_pat.findall(text1))
+text2 = 'Computer says "no." Phone says "yes."'
+print(str_pat.findall(text2))
+str_pat = re.compile(r'\"(.*?)\"')
+print(str_pat.findall(text2))
