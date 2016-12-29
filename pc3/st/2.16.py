4d6a6f69a95a9bdc4d33ea2294609503b6df2b17
Reid <reidddchan@outlook.com>
2016-12-30 01:09:02 +0800

    PYTHON-00040 PC3-02: Reformatting Text to a Fixed Number of Columns

---
+++ pc3/st/2.16.py
@@ -0,0 +1,13 @@
+"""
+Reformatting Text to a Fixed Number of Columns
+"""
+import textwrap
+import os
+
+s = "Look into my eyes, look into my eyes, the eyes, the eyes, \
+the eyes, not around the eyes, don't look around the eyes, \
+look into my eyes, you're under."
+print(textwrap.fill(s, 70))
+print(textwrap.fill(s, 40))
+print(textwrap.fill(s, 40, initial_indent='    '))
+print(textwrap.fill(s, 40, subsequent_indent='    '))
